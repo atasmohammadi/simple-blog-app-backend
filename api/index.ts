e8bd0cb307e7e701cd3ab3d81d81ae34335956c6
Ata Mohammadi <atasmohammadi@gmail.com>
2024-05-30 17:06:08 +0200

    fix cors issue

--- api/index.ts
+++ api/index.ts
@@ -26,6 +26,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors());
 app.use(bodyParser.json());
 
 // Define routes
--- api/models/database.ts
+++ api/models/database.ts
@@ -3,18 +3,38 @@ import path from "path";
 
 const DATA_PATH = path.join(__dirname, "../data");
 
-async function readDbFile(filename: string) {
+async function ensureDataDirExists() {
   try {
-    const filePath = path.join(DATA_PATH, filename);
+    await fs.ensureDir(DATA_PATH); // Create data directory if it doesn't exist
+  } catch (error) {
+    // Ignore errors if directory already exists
+    if (error.code !== "EEXIST") {
+      console.error("Error creating data directory:", error);
+      throw error;
+    }
+  }
+}
+
+async function readDbFile(filename: string) {
+  await ensureDataDirExists(); // Ensure data directory exists
+  const filePath = path.join(DATA_PATH, filename);
+  try {
     const data = await fs.readJson(filePath);
     return data;
   } catch (error) {
-    console.error(`Error reading the database file: ${filename}`, error);
-    throw error;
+    // If file doesn't exist, create an empty file and return null
+    if (error.code === "ENOENT") {
+      await fs.writeJson(filePath, {}, { spaces: 2 }); // Create empty file
+      return null;
+    } else {
+      console.error(`Error reading the database file: ${filename}`, error);
+      throw error;
+    }
   }
 }
 
 async function writeDbFile(filename: string, data: any) {
+  await ensureDataDirExists(); // Ensure data directory exists
   try {
     const filePath = path.join(DATA_PATH, filename);
     await fs.writeJson(filePath, data, { spaces: 2 });
